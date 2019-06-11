e7dc24dd51807b32c7563df4be2816a0f059aed5
siva <you@example.com>
2019-06-11 14:00:41 +0530

    personal details added validations

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,63 +1,52 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form'
+import { renderInput, renderTextArea } from '../globalComponents/formComponents';
+import { email, required, alphaNumeric, phoneNumber, maxLength } from '../globalComponents/validations';
+
 
 
 class PersonalDetails extends React.Component {
 
-
-  renderInput = ({ input, placeholder, type, meta }) => {
-    return <div className="ui input">
-      <input type={type || "text"} placeholder={placeholder} {...input} />
-      <div>{meta.error}</div>
-    </div>
-  }
-
-  renderTextArea = ({ input, placeholder, type, meta }) => {
-    return <div className="ui form textarea">
-      <textarea type={type || "text"} placeholder={placeholder} rows={4} cols={40} {...input} />
-      <div>{meta.error}</div>
-
-    </div>
-  }
-
   onSubmit = (formValues) => {
-    console.log('form value', formValues)
+    console.log('form valuessssssssssssssss', formValues)
+    this.props.reset()
   }
 
   render() {
+    const { submitting } = this.props
     return (
       <React.Fragment>
         <div className="ui text container">
           <h4 className="ui center aligned header">Personal Details</h4>
-          <form onSubmit={this.onSubmit} className="ui form">
+          <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
             <div className="ui segments">
               <div className="ui segment">
-                <div class="ui input focus">
-                  <Field name="name" component={this.renderInput} placeholder="Enter Name" />
+                <div className="ui input focus">
+                  <Field name="name" validate={required} component={renderInput} warn={alphaNumeric} placeholder="Enter Name" />
                 </div>
               </div>
               <div className="ui segment">
-                <Field name="role" component={this.renderInput} placeholder="Enter Role" />
+                <Field name="role" validate={required} component={renderInput} placeholder="Enter Role" />
 
               </div>
               <div className="ui segment">
-                <Field name="email" component={this.renderInput} placeholder="Enter Email" />
+                <Field name="email" validate={[required, email]} component={renderInput} placeholder="Enter Email" />
 
               </div>
               <div className="ui segment">
-                <Field name="mobile" component={this.renderInput} placeholder="Enter Mobile" />
+                <Field name="mobile" validate={[required, phoneNumber]} component={renderInput} placeholder="Enter Mobile" />
 
               </div>
               <div className="ui segment">
-                <Field name="address" component={this.renderTextArea} placeholder="Enter Address" />
+                <Field name="address" validate={required} component={renderTextArea} warn={maxLength(250)} placeholder="Enter Address" />
 
               </div>
               <div className="ui segment">
-                <Field name="summary" component={this.renderTextArea} placeholder="Enter Summary" />
+                <Field name="summary" validate={required} component={renderTextArea} warn={maxLength(250)} placeholder="Enter Summary" />
               </div>
 
               <div className="ui segment">
-                <button type="submit" class="ui primary button">
+                <button type="submit" className="ui primary button" disabled={submitting}>
                   Submit
                 </button>
               </div>
--- src/globalComponents/formComponents.js
+++ src/globalComponents/formComponents.js
@@ -1,15 +1,15 @@
 
-
-export const renderInput = ({ input, placeholder, type, meta }) => {
-  return <div className="ui input">
+import React from 'react'
+export const renderInput = ({ input, placeholder, type, meta: { touched, error, warning } }) => {
+  return <><div className="ui input">
     <input type={type || "text"} placeholder={placeholder} {...input} />
-    <div>{meta.error}</div>
   </div>
+    {touched && ((error && <div className="ui left pointing red basic label meta_div">{error}</div>) || (warning && <div className="ui left pointing yellow basic label meta_div">{warning}</div>))}</>
 }
 
-export const renderTextArea = ({ input, placeholder, type, meta }) => {
-  return <div className="ui form textarea">
+export const renderTextArea = ({ input, placeholder, type, meta: { touched, error, warning } }) => {
+  return <><div className="ui form textarea">
     <textarea type={type || "text"} placeholder={placeholder} rows={4} cols={40} {...input} />
-    <div>{meta.error}</div>
   </div>
+    {touched && ((error && <div className="ui top pointing red basic label meta_div">{error}</div>) || (warning && <div className="ui left pointing yellow basic label meta_div">{warning}</div>))}</>
 }
